2a99fbfaba38b011d4bcc5e89152c8c6d8c7c76f
Agung Yuliyanto <agung.96tm@gmail.com>
2023-08-30 17:05:54 +0700

    add react-loading-skeleton package + restructure styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
+import "./styles/rootStyles";
+
 function App() {
   return (
     <StrictMode>
---
+++ src/styles/rootStyles.ts
@@ -0,0 +1,2 @@
+import "react-loading-skeleton/dist/skeleton.css";
+import "./rootStyles.css";
